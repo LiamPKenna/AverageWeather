7df2e9e983fade44a1995b4913eb2ff877b47580
LiamPKenna <liampkenna@gmail.com>
2019-11-02 12:31:48 -0700

    move cities to seperate file with example cities to show data needed

---
+++ cities.js
@@ -0,0 +1,13 @@
+// Example cities, feel free to swap out with whatever suits your needs.
+
+const cities = [
+  ['Portland OR', '45.5155,-122.6793', '47'],
+  ['New York NY', '40.7128,-74.0060', '49'],
+  ['Los Angeles CA', '34.0522,-118.2437', '50'],
+  ['Seattle WA', '47.6062,-122.3321', '48'],
+  ['New Orleans LA', '29.9511,-90.0715', '51'],
+  ['Chicago IL', '41.8781,-87.6298','52']
+];
+
+
+module.exports = cities;
